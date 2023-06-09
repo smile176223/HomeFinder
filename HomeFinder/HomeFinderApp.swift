353bd211fd00b5afa59bbe21b12025b20cf528fa
LiamHsu <s459958@gmail.com>
2023-06-09 15:08:18 +0800

    init xcode project.

---
+++ HomeFinder/HomeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeFinderApp.swift
+//  HomeFinder
+//
+//  Created by Liam on 2023/6/9.
+//
+
+import SwiftUI
+
+@main
+struct HomeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
